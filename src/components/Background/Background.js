bfa472aec019151851f859bf25edb86b4f8261c1
XXLZopes <afordm99@gmail.com>
2022-07-15 15:13:24 -0700

    testing it outttttt

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -45,20 +45,40 @@ export default function Background ({navState, setNavState}) {
         }
     })
 
+    // class Circle {
+    //     constructor(x, y, r, color, text) {
+    //         this.x = dimensions.width / x || 0;
+    //         this.y = dimensions.height / y || 0;
+    //         this.r = dimensions.width <= 700 ? dimensions.width / r : 700 / r;
+    //         this.color = color
+    //         this.text = text || ''
+    //     }
+    //     move(x, y) {
+    //         this.x = dimensions.width / x || 0;
+    //         this.y = dimensions.height / y || 0;
+    //     }
+    //     setRadius(r) {
+    //         this.r = dimensions.width <= 700 ? dimensions.width / r : 700 / r;
+    //     }
+    //     setText(text) {
+    //         this.text = text || ''
+    //     }
+    // }
+    
     class Circle {
         constructor(x, y, r, color, text) {
-            this.x = dimensions.width / x || 0;
-            this.y = dimensions.height / y || 0;
-            this.r = dimensions.width <= 700 ? dimensions.width / r : 700 / r;
+            this.x = x;
+            this.y = y;
+            this.r = window.innerWidth < 700 ? r : dimensions.width / 200 * parseInt(r.split('')[0] + r.split('')[1]);
             this.color = color
             this.text = text || ''
         }
         move(x, y) {
-            this.x = dimensions.width / x || 0;
-            this.y = dimensions.height / y || 0;
+            this.x = x;
+            this.y = y;
         }
         setRadius(r) {
-            this.r = dimensions.width <= 700 ? dimensions.width / r : 700 / r;
+            this.r = r;
         }
         setText(text) {
             this.text = text || ''
@@ -68,37 +88,46 @@ export default function Background ({navState, setNavState}) {
     let left_ = dimensions.width
     let bottom_ = 1.11
 
-    let circle0 = new Circle(1.215, 25, 5.7, '#FDFFB2', 'Home')
-    let circle1 = new Circle(1.7, 3.5, 4.6, '#96DFFF', 'About Me')
-    let circle2 = new Circle(left_, 2.7, 2.75, '#FF8B8B', 'Past Projects')
-    let circle3 = new Circle(3, 1.5, 5.7, '#7451FF', 'Contact')
-    let circle4 = new Circle(1.25, 1.25, 11, '#B6FF9C')
-    let circle5 = new Circle(left_, bottom_, 6, '#B3B3B3')
+    // let circle0 = new Circle(1.215, 25, 5.7, '#FDFFB2', 'Home')
+    // let circle1 = new Circle(1.7, 3.5, 4.6, '#96DFFF', 'About Me')
+    // let circle2 = new Circle(left_, 2.7, 2.75, '#FF8B8B', 'Past Projects')
+    // let circle3 = new Circle(3, 1.5, 5.7, '#7451FF', 'Contact')
+    // let circle4 = new Circle(1.25, 1.25, 11, '#B6FF9C')
+    // let circle5 = new Circle(left_, bottom_, 6, '#B3B3B3')
+
+    let circle0 = new Circle('81vw', '4vh', '19vw', '#FDFFB2', 'Home')
+    let circle1 = new Circle('59vw', '30vh', '23vw', '#96DFFF', 'About Me')
+    let circle2 = new Circle('0vw', '35vh', '35vw', '#FF8B8B', 'Past Projects')
+    let circle3 = new Circle('37vw', '65vh', '19vw', '#7451FF', 'Contact')
+    let circle4 = new Circle('76vw', '85vh', '9vw', '#B6FF9C')
+    let circle5 = new Circle('0vw', '90.3vh', '17vw', '#B3B3B3')
 
     function setOpenNavCircleCoords() {
-        circle0.move(4, 25)
-        circle1.move(2, 5)
-        circle2.move(7, 3)
-        circle3.move(2.2, 1.65)
+        circle0.move('15vw', '8vh')
+        circle1.move('50vw', '25vh')
+        circle2.move('15vw', '40vh')
+        circle3.move('45vw', '70vh')
         // circle4.move(1.25, 1.3)
 
-        circle0.setRadius(5)
-        circle1.setRadius(4)
-        circle2.setRadius(2.2)
-        circle3.setRadius(5)
-        // circle4.setRadius(8.5)
-        // circle5.setRadius(5.5)
+        circle0.setRadius('19vw')
+        circle1.setRadius('23vw')
+        circle2.setRadius('35vw')
+        circle3.setRadius('19vw')
     }
 
-    function setCloseNavCircleCoords() {
-        circle0.move(1.215, 25)
-        circle1.move(1.7, 3.5)
-        circle2.move(left_, 2.7)
-        circle3.move(3, 1.5)
-        circle4.move(1.25, 1.25)
+    // function setCloseNavCircleCoords() {
+        // circle0.move('81vw', '4vh')
+        // circle1.move('59vw', '30vh')
+        // circle2.move('0vw', '35vh')
+        // circle3.move('37vw', '65vh')
+        // circle4.move('76vw', '85vh')
+    // }
+
+    if (navState) {
+        setOpenNavCircleCoords()
     }
 
-    navState ? setOpenNavCircleCoords() : setCloseNavCircleCoords();
+    // navState ? setOpenNavCircleCoords() : setCloseNavCircleCoords();
     
     const circlesNav = [circle0, circle1, circle2, circle3]
     const circlesOther = [circle4, circle5]
